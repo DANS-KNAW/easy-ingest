b84b52aa69d6a7182816f51ac5511371d1ff7418
Paul Boon <paul.boon@dans.knaw.nl>
2016-06-09 16:48:31 +0200

    refactoring

--- src/main/scala/nl/knaw/dans/easy/ingest/EasyIngest.scala
+++ src/main/scala/nl/knaw/dans/easy/ingest/EasyIngest.scala
@@ -20,7 +20,7 @@ import java.net.URI
 
 import com.yourmediashelf.fedora.client.FedoraClient
 import com.yourmediashelf.fedora.client.FedoraClient._
-import com.yourmediashelf.fedora.client.request.FedoraRequest
+import com.yourmediashelf.fedora.client.request.{AddDatastream, FedoraRequest}
 import org.apache.commons.configuration.PropertiesConfiguration
 import org.apache.commons.io.FileUtils
 import org.apache.commons.lang.exception.ExceptionUtils._
@@ -163,23 +163,26 @@ object EasyIngest {
     if (dsSpec.dsLocation != "") {
       request = request.dsLocation(dsSpec.dsLocation)
     } else if (dsSpec.contentFile != "") {
-      request = sdo.listFiles.find(_.getName == dsSpec.contentFile) match {
-        case Some(file) =>
-          if (datastreamId == "EMD") {
-            val placeholder = "$sdo-id"
-            val tmpFile = File.createTempFile(file.getName, null)
-            // Note that this would change the file's checksum, but it is only for the EMD datastream, which has no checksum
-            replacePlaceholderInFileCopy(file, tmpFile, placeholder, pidDictionary(sdo.getName))
-            request.content(tmpFile)
-            val location = request.execute().getLocation
-            FileUtils.deleteQuietly(tmpFile)
-            return Success(location)
-          }
-          request.content(file)
-        case None => throw new RuntimeException(s"Couldn't find specified datastream: ${dsSpec.contentFile}")
+      request = (datastreamId, sdo.listFiles.find(_.getName == dsSpec.contentFile)) match {
+        case ("EMD", Some(file)) =>
+          // Note that this would change the ingested file's checksum, but it is only for the EMD datastream, which has no checksum
+          return Success(executeAddRequestWithReplacement(request, file, "$sdo-id", pidDictionary(sdo.getName)))
+        case (_, Some(file)) => request.content(file)
+        case (_, None) => throw new RuntimeException(s"Couldn't find specified datastream: ${dsSpec.contentFile}")
       }
     }
     request.execute().getLocation
+  }
+
+  private def executeAddRequestWithReplacement(request: AddDatastream, file: File, placeholder: String, replacement:String): URI = {
+    val tmpFile = File.createTempFile(file.getName, null)
+    log.debug(s"Created temp file: '$tmpFile.getAbsolutePath'")
+    replacePlaceholderInFileCopy(file, tmpFile, placeholder, replacement)
+    request.content(tmpFile)
+    val location = request.execute().getLocation
+    log.debug(s"Deleting temp file: '$tmpFile.getAbsolutePath'")
+    FileUtils.deleteQuietly(tmpFile)
+    location
   }
 
   private def replacePlaceholderInFileCopy(src:File, dst:File, placeholder:String, replacement:String): Unit = {
