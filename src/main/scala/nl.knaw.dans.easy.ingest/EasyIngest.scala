30823f8914afb1387e9fb6b63e3dfc8d227d23cb
Richard van Heest <richard.van.heest@dans.knaw.nl>
2017-09-25 13:35:18 +0200

    only add label when the string isn't empty

--- src/main/scala/nl.knaw.dans.easy.ingest/EasyIngest.scala
+++ src/main/scala/nl.knaw.dans.easy.ingest/EasyIngest.scala
@@ -188,13 +188,15 @@ object EasyIngest extends DebugEnhancedLogging {
     logger.debug(s"Adding datastream with dsId = $datastreamId")
     val requestBase = FedoraClient.addDatastream(pid, datastreamId)
       .versionable(false)
-      .dsLabel(dsSpec.label)
       .mimeType(dsSpec.mimeType)
       .controlGroup(dsSpec.controlGroup)
 
-    val requestBase2 = if (dsSpec.checksumType.nonEmpty && dsSpec.checksum.nonEmpty)
-                         requestBase.checksumType(dsSpec.checksumType).checksum(dsSpec.checksum)
+    val requestBase1 = if (dsSpec.label.nonEmpty) requestBase.dsLabel(dsSpec.label)
                        else requestBase
+
+    val requestBase2 = if (dsSpec.checksumType.nonEmpty && dsSpec.checksum.nonEmpty)
+                         requestBase1.checksumType(dsSpec.checksumType).checksum(dsSpec.checksum)
+                       else requestBase1
 
     val request =
       if (dsSpec.dsLocation.nonEmpty)
