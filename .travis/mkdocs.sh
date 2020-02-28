1aede486e394359dc7a5f4ae19c20fafcf655d94
Jan van Mansum <jan.van.mansum@dans.knaw.nl>
2020-02-28 18:23:14 +0100

    Refactored mkdocs site generation

--- .travis/mkdocs.sh
+++ .travis/mkdocs.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #
-# Copyright (C) 2018 DANS - Data Archiving and Networked Services (info@dans.knaw.nl)
+# Copyright (C) 2016 DANS - Data Archiving and Networked Services (info@dans.knaw.nl)
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -24,13 +24,7 @@ GH_REPO=$(basename $TRAVIS_BUILD_DIR)
 REMOTE="https://${GH_TOKEN}@github.com/${GH_ORG}/${GH_REPO}"
 git remote set-url origin ${REMOTE}
 
-echo "START installing required Python packages..."
-pip3 install mkdocs
-pip3 install pygments
-pip3 install pymdown-extensions
-pip3 install pyyaml
-pip3 install mkdocs-markdownextradata-plugin
-echo "DONE installing required Python packages."
+pip install -r .travis/requirements.txt
 
 echo "START installing DANS mkdocs theme..."
 git clone https://github.com/Dans-labs/mkdocs-dans $HOME/mkdocs-dans
